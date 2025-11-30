86a80775dfbc70f0ed30e4377a301ffb686d1b4c
Jeanelle_Eshun <jeanelle.eshun@gmail.com>
2025-11-30 16:41:29 -0500

    input of table and grades

--- lab-02/StudentGrades.java
+++ lab-02/StudentGrades.java
@@ -6,5 +6,22 @@
 
 public class StudentGrades {
   public static void main(String[] args) {
+ int jTotal = 30 + 15;
+ int joeTotal = 43 + 7;
+ int wilTotal = 50 + 8;
+ int mTotal = 39 + 10;
+ int sTotal = 50 + 15;
+
+    System.out.println("///////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\n" + //
+            "==          Student Points          ==\n" + //
+            "\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\///////////////////");
+
+System.out.println("   ");
+System.out.println("NAME       LAB       BONUS       TOTAL");
+System.out.println("Jeremy       30        15"       + jTotal);
+System.out.println("Joe          43         7"         +joeTotal);
+System.out.println("William      50         8"         +wilTotal);
+System.out.println("Mary Sue     39        10"      +   mTotal);
+System.out.println("Samuel       50        15"         +sTotal);
   }
 }
