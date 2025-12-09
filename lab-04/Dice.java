d4f216199603c2a1d73074dcd77ca88a80373ae6
Jeanelle_Eshun <jeanelle.eshun@gmail.com>
2025-12-08 20:47:40 -0500

    die print statement

--- lab-04/Dice.java
+++ lab-04/Dice.java
@@ -8,5 +8,11 @@ import java.util.Random;
 
 public class Dice {
   public static void main(String[] args) {
+    Random number = new Random();
+    int a = number.nextInt(6) + 1;
+    int b = number.nextInt(6) + 1;
+    System.out.println("The number from die one is : "+ a);
+    System.out.println("The number from die two is: " + b);
+    System.out.println("The total of both dies is: " +  (a + b));
   }
 }
